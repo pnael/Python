8164c814a56cdb8ee7cd46e1579cd7b73757af37
Philippe NAEL <philippe.nael@gmail.com>
2019-10-28 13:18:27 +0100

    Cleaning old stuff

--- libturpial-1.7.0/libturpial/lib/protocols/twitter/params.py
+++
@@ -1,7 +0,0 @@
-OAUTH_OPTIONS = {
-    'consumer_key': 'z6KPZZDCsOkKybmgCjlznw',
-    'consumer_secret': 'h4Ox3DmAstfO4l2WKf5T55MHGNjyJG80b0MJfkQ0',
-    'request_token_url': 'https://api.twitter.com/oauth/request_token',
-    'access_token_url': 'https://api.twitter.com/oauth/access_token',
-    'authorize_token_url': 'https://api.twitter.com/oauth/authorize',
-}
