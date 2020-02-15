6914410c48cb052abced57f43d0440aa8f023363
Philippe NAEL <philippe.nael@gmail.com>
2020-02-15 09:41:05 +0100

    Cleaned ?

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Petit programme qui calcule la suite de Fibonacci
 
-a,b,c = 1,1,1
+a,b = 0,1
 
-print 1
-
+while a < 1000:
+    print(a, end=",")
+    a,b = b, a+b
+print()
