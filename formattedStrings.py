a43814b909b4222f4f8861984c21b82f6a883f4a
Philippe NAEL <philippe.nael@gmail.com>
2020-02-26 17:58:33 +0100

    Learning

---
+++ formattedStrings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+if __name__=="__main__":
+    fistname = 'Philippe'
+    lastname = 'Naël'
+
+    msg = f'{fistname} {lastname} learns coding'
+
+    print(msg)
---
+++ loop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+if __name__=="__main__":
+    x = 10
+    x += 1
+    print(x)
