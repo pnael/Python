0b4b1a28af887f6c72079595e962aa366bef9bc5
Philippe NAEL <philippe.nael@gmail.com>
2020-03-21 18:34:33 +0100

    Working ping

---
+++ ping.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+for i in range(254):
+    ip = f'192.168.1.{i}'
+    response = os.system("ping -c 1 " + ip+">/dev/null 2>&1")
+    if response == 0:
+        print(ip, 'is up!')
+    else:
+        print(ip, 'is down!')
