f40b37d547d5cf05b8ae7b166ff4d6e1b9b286d2
Philippe NAEL <philippe.nael@gmail.com>
2020-10-10 09:50:21 +0200

    Learning rest

--- PycharmProjects/rest_json/main.py
+++ PycharmProjects/rest_json/main.py
@@ -1,8 +1,10 @@
 import requests
 
-url = 'https://www.w3schools.com/python/demopage.php'
+url = 'https://www.owenscorning.com/en-us'
 myobj = {'somekey': 'somevalue'}
 
 x = requests.post(url, data = myobj)
-
+print(x.url)
+print(x.encoding)
+print(x.headers)
 print(x.text)
